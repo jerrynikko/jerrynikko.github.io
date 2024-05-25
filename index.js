495f727cd9950b67e84fdeff5fa6d114a105aef2
jerrynikko <jerrynikkolacaba@gmail.com>
2024-05-25 14:47:19 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 const btnLikes1 = document.getElementById("btnLike1")
 const countLikes1 = document.getElementById("countLikes1")
+
+function clickLike1(){
+  let totalLikes1 = parseInt(countLikes1.value) + 1
+  countLikes1.textContent = totalLikes1.toString()
+}
+btnLike1.addEventListener("click",clickLike1)
