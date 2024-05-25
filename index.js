21ac40f0e88e15b7589c3870249d1921edc88a52
jerrynikko <jerrynikkolacaba@gmail.com>
2024-05-25 15:49:22 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const btnLikes1 = document.getElementById("btnLike1")
 const countLikes1 = document.getElementById("countLikes1")
+const btnDislikes1 = document.getElementById("btnDislike1")
+const countDislikes1 = document.getElementById("countDislikes1")
 const btnLikes2 = document.getElementById("btnLike2")
 const countLikes2 = document.getElementById("countLikes2")
+
 
 function clickLike1(){
   let totalLikes1 = parseInt(countLikes1.value) + 1
   countLikes1.textContent = totalLikes1.toString()
+}
+
+function clickDislike1(){
+  let totalDislikes1 = parseInt(countDislikes1.value) + 1
+  countDislikes1.textContent = totalDislikes1.toString()
 }
 
 function clickLike2(){
